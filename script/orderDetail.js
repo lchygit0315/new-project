c663abfbd2118b6fade257e5c41a3cc9b092aa95
lcy <lchy_1989@163.com>
2018-06-21 17:44:25 +0800

    更新页面

---
+++ script/orderDetail.js
@@ -0,0 +1,14 @@
+$(function () {
+    //tab切换
+    $(".page-order-box>div").hide().first().show();
+    $(".page-order-menu ul li").on("click",function(){
+        var _index = $(this).index();
+        $(".page-order-box>div").eq(_index).show().siblings().hide();
+        $(this).addClass("active").siblings().removeClass("active");
+        if(_index==0){
+            $('.order-line').animate({'left':'16%'},300);
+        }else{
+            $('.order-line').animate({'left':'64%'},300);
+        }
+    });
+})
